2f1055ce00d233008ef72fbb1de570c14fc932ed
Romain Gaudel <romaingaudel83@gmail.com>
2022-09-13 17:14:14 +0200

    Appel Api methode fetch then + DOM

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,14 +1,43 @@
-/* Appel de l'API */
-const urlApi = "http://localhost:3000/api/products";
+// Appel the API
+const api = "http://localhost:3000/api/products/";
 
-fetch(urlApi)
-.then((resp) => resp.json())
-.then(function(data){
-  let canapes = data.products;
-  return canapes.map(function(canapes){
-    let link = document.createElement("a");
-    let img = document.createElement("img");
-    let span = createNode('span');
-    img.src = author.picture.medium;
-  })
+function affichageKanap () {
+      fetch(api)
+      .then(function (response) {
+            if (response.ok) {
+                  return response.json();
+            }
+      })
+      .then(function (canapes) {
+            console.log(canapes);
+            canapes.forEach(canape => {
+                  console.log(canapes);
+
+            const lien = document.createElement("a");
+            items.appendChild(lien);
+            lien.setAttribute("href", `product.html?id=${canape._id}`);
+      
+            const article = document.createElement("article");
+            lien.appendChild(article);
+
+            const img = document.createElement("img");
+            article.appendChild(img);
+            img.setAttribute("src", canape.imageUrl);
+            img.setAttribute("alt", canape.altTxt);
+
+            const title = document.createElement("h3");
+            article.appendChild(title);
+            title.innerText = canape.name;
+
+            const description = document.createElement("p");
+            article.appendChild(description);
+            description.innerText = canape.description;
+            
+      });
+
 })
+.catch(function (error) {
+  alert("⚠️ Error! Fetch()!")
+});
+}
+affichageKanap();
