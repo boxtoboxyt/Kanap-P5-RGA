24d1bf5e302ff70502ff6540e65e84b9b3ccd18f
Romain Gaudel <romaingaudel83@gmail.com>
2022-09-21 17:38:33 +0200

    finalisation product.js

--- front/JS/product.js
+++ front/JS/product.js
@@ -36,20 +36,40 @@ fetch(`http://localhost:3000/api/products/${id}`)
 
 produitKanap();
 
-const addCart = document.getElementById("addToCart");
+const addToCart = document.getElementById("addToCart");
 const quantity = document.getElementById("quantity");
 const colors = document.getElementById("colors");
 
-const panier = [id, quantity, colors];
+addToCart.addEventListener("click", function (){
 
-function pushKanap () {
-addCart.addEventListener("click", function(event) {
-    let localStorageKanap = localStorage.getItem("products");
-    let kanapJson = JSON.parse(localStorageKanap);
-    let cartKanap = {
-        id: kanapJson.productId,
-        choixQuantite: quantity.value,
-        choixCouleur: colors.value
+    let kanapDetails = {
+        Id: id,
+        Color: colors.value,
+        Quantity: quantity.value
     }
 
-})};
+    let kanapInfos = JSON.parse(localStorage.getItem("Kanap"));
+     //Si le panier comporte déjà au moins 1 article
+    if (kanapInfos) {
+        const resultFind = kanapInfos.find(
+            (el) => el.Id === id && el.Color === colors.value);
+            //Si le produit commandé est déjà dans le panier
+            if (resultFind) {
+                let newQuantite =
+                parseInt(kanapDetails.Quantity) + parseInt(resultFind.Quantity);
+                resultFind.Quantity = newQuantite;
+                localStorage.setItem("Kanap", JSON.stringify(kanapInfos));
+            //Si le produit commandé n'est pas dans le panier
+            } else {
+                kanapInfos.push(kanapDetails);
+                localStorage.setItem("Kanap", JSON.stringify(kanapInfos));
+            }
+        //Si le panier est vide
+        } else {
+            kanapInfos =[];
+            kanapInfos.push(kanapDetails);
+            localStorage.setItem("Kanap", JSON.stringify(kanapInfos));
+    }
+})
+
+
